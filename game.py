9794ed5914fb939319a8c5d9498c0393e9631f4d
yk <jerr8538@gmail.com>
2025-04-09 16:02:52 +0530

    done fr

--- game.py
+++ game.py
@@ -10,22 +10,37 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 running = True
 
-phineas_image = pygame.image.load("assets/images/phineas_flipped.png")
+p_l = pygame.image.load("assets/images/phineas_l.png")
+p_r = pygame.image.load("assets/images/phineas_r.png")
+phineas_image = p_r
+i_r = pygame.image.load("assets/images/isabella_r.png")
+i_l = pygame.image.load("assets/images/isabella_l.png")
+isabella_image = i_r
 
-phineas_x = 0
-phineas_y = 0
+p_x = 0
+p_y = 0
 
-phineas_vx = 20
-phineas_vy = 0
+i_x = 0
+i_y = 0
+
+p_vx = 20
+p_vy = 0
+
+i_vx = 40
+i_vy = 0
 
 gravity = 500
 
 p_width = phineas_image.get_width()
 p_height = phineas_image.get_height()
 
+i_width = isabella_image.get_width()
+i_height = isabella_image.get_height()
+
 clock = pygame.time.Clock()
 
-rects = []
+p_rects = []
+i_rects = []
 
 score = 0
 
@@ -39,42 +54,88 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_SPACE]:
-        phineas_vy = -400
+    if keys[pygame.K_UP]:
+        p_vy = -400
+    if keys[pygame.K_COMMA]:
+        i_vy = -400
 
     if keys[pygame.K_RIGHT]:
-        phineas_x += phineas_vx
+        p_x += p_vx
+        phineas_image = p_r
     if keys[pygame.K_LEFT]:
-        phineas_x -= phineas_vx
+        p_x -= p_vx
+        phineas_image = p_l
+    if keys[pygame.K_DOWN]:
+        p_y += 100
+    if keys[pygame.K_e]:
+        i_x += i_vx
+        isabella_image = i_l
+    if keys[pygame.K_a]:
+        i_x -= i_vx
+        isabella_image = i_r
+    if keys[pygame.K_o]:
+        i_y += 100
 
     screen.fill((0, 0, 0))
 
-    if random.randint(0, 2000) > 1897:
-        rects.append(pygame.rect.Rect(WIDTH, random.randint(0, HEIGHT), 10, 100))
-    
-    for rect in rects[:]:
+    if random.randint(0, 2000) > 1900:
+        p_rects.append(pygame.rect.Rect(WIDTH, random.randint(0, HEIGHT), 10, 100))
+    if random.randint(0, 2000) > 1900:
+        i_rects.append(pygame.rect.Rect(WIDTH, random.randint(0, HEIGHT), 10, 100))
+
+    for rect in p_rects[:]:
         pygame.draw.rect(screen, (0, 255, 0), rect)
+    for rect in i_rects[:]:
+        pygame.draw.rect(screen, (255, 192, 203), rect)
 
-    phineas_rect = pygame.rect.Rect(phineas_x, phineas_y, p_width, p_height)
+    p_rect = pygame.rect.Rect(p_x, p_y, p_width, p_height)
+    i_rect = pygame.rect.Rect(i_x, i_y, i_width, i_height)
 
-    for rect in rects:
-        if phineas_rect.colliderect(rect):
-            # pygame.draw.rect(screen, (255, 0, 0), rect)
-            # print("collided")
-            rects.remove(rect)
+    for rect in p_rects:
+        if p_rect.colliderect(rect):
+            p_rects.remove(rect)
             score += 1
-            
+        rect.x -= 5
+    
+    for rect in i_rects:
+        if i_rect.colliderect(rect):
+            i_rects.remove(rect)
+            score += 1
+
         rect.x -= 5
 
-    screen.blit(phineas_image, (phineas_x, phineas_y))
+    screen.blit(phineas_image, (p_x, p_y))
+    screen.blit(isabella_image, (i_x, i_y))
 
-    phineas_vy += gravity * dt
+    p_vy += gravity * dt
+    i_vy += gravity * dt
 
-    phineas_y += phineas_vy * dt
+    p_y += p_vy * dt
+    i_y += i_vy * dt
 
-    if phineas_y > HEIGHT - p_height:
-        phineas_y = HEIGHT - p_height
-        phineas_vy = 0
+    if p_y < 0:
+        p_y = 0
+        p_vy = 100
+    if i_y < 0:
+        i_y = 0
+        i_vy = 100
+
+    if p_y > HEIGHT - p_height:
+        p_y = HEIGHT - p_height
+        p_vy = 0
+    if i_y > HEIGHT - i_height:
+        i_y = HEIGHT - i_height
+        i_vy = 0
+
+    if p_x < 0:
+        p_x = 0
+    if i_x < 0:
+        i_x = 0
+
+    if p_x > WIDTH - p_width:
+        p_x = WIDTH - p_width
+    if i_x > WIDTH - i_width:
+        i_x = WIDTH - i_width
 
     pygame.display.flip()
 
